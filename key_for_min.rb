efb49a3d6446a525b3d12b06d878fcfb5dc05b56
Jamal Shareef <theepie@hotmail.com>
2019-04-04 20:42:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   if name_hash == {}
     nil
   end
-    min = INFINITY
+    min = Float::INFINITY
     name_hash.each do |key, value|
       if value < min
         min = value
