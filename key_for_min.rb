046e050408d1f200b5c0f2f0917839c64007b547
Jamal Shareef <theepie@hotmail.com>
2019-04-04 20:35:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,13 @@ def key_for_min_value(name_hash)
   if name_hash == {}
     nil
   else
-    min = infinit
+    min = INFINITY
     name_hash.each do |key, value|
+      if value < min
+        min = value
+        pair = {key: value}
+      end
     end
-    
+  end
+  pair
 end
