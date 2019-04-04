897bb4c73baf13a6a342d2cae8697e2817847a31
Jamal Shareef <theepie@hotmail.com>
2019-04-04 20:39:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,8 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+INFINITY
+
 def key_for_min_value(name_hash)
   if name_hash == {}
     nil
